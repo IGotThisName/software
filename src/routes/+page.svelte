18b97c0e0e9cd88f496850202cd2c6905ef7423b
IGotThisName <eric.burakowski@gmail.com>
2025-05-05 09:47:58 +1000

    Blob test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,8 +23,23 @@
 {#await fetchData()}
 <p>Loading...</p>
 {:then}
-<textarea bind:value={md}></textarea>
-<div class="prose">
-	<Markdown {md} />
+
+<div class="flex flex-row h-screen w-screen">
+  <textarea 
+    bind:value={md}
+    class="flex w-1/2 p-4 resize-none outline-none focus:bg-slate-50"
+  ></textarea>
+  <div class="prose p-4">
+    <Markdown {md} />
+  </div>
 </div>
+
+<div class="fixed bottom-4 right-4">
+  <button
+    class="bg-slate-200 w-20 h-12 
+    flex justify-center items-center 
+    rounded-md shadow-md hover:bg-slate-300 transition-all"
+  >Save</button>
+</div>
+
 {/await}
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,16 +1,17 @@
 import { json } from "@sveltejs/kit";
+import { getDownloadUrl, head } from "@vercel/blob";
+import { BLOB_READ_WRITE_TOKEN } from '$env/static/private';
 
 export async function GET() {
 
-  await delay(1000);
+  console.log("begin blob fetch")
+  const blob = await head('markdown/test.md', {token:BLOB_READ_WRITE_TOKEN});
+  console.log("begin markdown fetch")
+  const md = await fetch(blob.downloadUrl);
 
   return json(
     {
-      'markdown': "# Hello World!"
+      'markdown': md
     }
   );
-}
-
-function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
 }
