6f60bd91ce013b6a6f2f6ec7810ae27252b7da53
IGotThisName <eric.burakowski@gmail.com>
2025-05-05 10:10:54 +1000

    Fixed fetching markdown file

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,5 +1,5 @@
 import { json } from "@sveltejs/kit";
-import { getDownloadUrl, head } from "@vercel/blob";
+import { head } from "@vercel/blob";
 import { BLOB_READ_WRITE_TOKEN } from '$env/static/private';
 
 export async function GET() {
@@ -7,7 +7,9 @@ export async function GET() {
   console.log("begin blob fetch")
   const blob = await head('markdown/test.md', {token:BLOB_READ_WRITE_TOKEN});
   console.log("begin markdown fetch")
-  const md = await fetch(blob.downloadUrl);
+
+  const resp = await fetch(blob.downloadUrl);
+  const md = await resp.text()
 
   return json(
     {
