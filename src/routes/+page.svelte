3d360d284073619ab8911a843d92e22ff77cd3b2
IGotThisName <eric.burakowski@gmail.com>
2025-05-13 14:42:39 +1000

    Basic styling to document list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
   const data = props.data;
 </script>
 
-<h1>Documents</h1>
-<ul>
-  {#each data.titles as title}
-    <ul><a href={'/document/' + title}>{title}</a></ul>
-  {/each}
-</ul>
+<div>
+  <h1 class="text-5xl mb-2">Documents</h1>
+  <ul class="ml-2 text-lg">
+    {#each data.titles as title}
+      <li><a href={'/document/' + title}>{title}</a></li>
+    {/each}
+  </ul>
+</div>
