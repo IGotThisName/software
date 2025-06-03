b7c0693f4d9c24eb0c4eae515a7172f8aa2eac48
IGotThisName <eric.burakowski@gmail.com>
2025-06-03 10:26:00 +1000

    dashboard progress

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,5 @@
 import { supabase } from "$lib/supabaseClient.js";
-import { redirect } from "@sveltejs/kit";
+import { error, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types.js";
 import type { Document } from "$lib/types.js";
 
@@ -33,3 +33,18 @@ export const load: PageServerLoad = async ({ locals }) => {
 
   return ({ documents, user })
 }
+
+export const actions = {
+  create: async (event) => {
+    const user = event.locals.user;
+
+    const { data: document, error: documentError } = await supabase.from('documents').insert({user: user?.id}).select().single();
+
+    if (document) {
+      redirect(308, '/document/' + document.id)
+    }
+  },
+  delete: async (event) => {
+    // TODO
+  }
+} satisfies Actions
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,14 +1,50 @@
-<script>
+<script lang='ts'>
+  import { enhance } from "$app/forms";
+
   const props = $props();
   const data = props.data;
+
+  let seldoc = $state(-1);
+  let sel = $state(-1);
+
+
+  const buttonStyle = "text-center shadow-lg rounded-md inset-shadow-slate-400/50 inset-shadow-sm p-2 transition-all hover:shadow-xl/25 hover:my-4 cursor-pointer";
 </script>
 
-<div class="p-4">
-  <h1 class="text-xl mb-2">User {data.user.username}</h1>
-  <ul class="ml-2 text-lg">
-    {#each data.documents as document}
-      <li><a href={'/document/' + document.id}>{document.title}</a></li>
+<div class="w-screen h-screen flex flex-row bg-slate-300">
+  
+  <div class="h-screen w-1/6 bg-slate-500 rounded-r-xl flex flex-col justify-center text-white inset-shadow-sm/50 shadow-sm/50">
+
+    <form class="text-4xl p-6 gap-4 flex flex-col" action="?/create" method="POST" use:enhance={({ formData }) => {
+      formData.append('id', seldoc.toString());
+
+      return async ({ update }) => {
+        update({ reset: false })
+      };
+    }}>
+      <a class={buttonStyle} href='/document/{seldoc}' >Open</a>
+      <button class={buttonStyle} formaction="?/delete" >Delete</button>
+      <button class={buttonStyle} formaction="?/create" >Create</button>
+    </form>
+
+  </div>
+
+  <div class="flex w-5/6 h-screen p-12 gap-12 flex-wrap justify-start overflow-scroll">
+
+    {#each data.documents as document, i}
+
+      <button 
+        onclick={() => {seldoc = document.id; sel = i}}
+        class='
+          {sel === i ? "border-4" : "border-none"}
+          size-96 bg-slate-400 cursor-pointer transition-all 
+          border-solid border-blue-700/50 rounded-lg
+        '
+      >
+        {document.title}
+      </button>
     {/each}
-  </ul>
+
+  </div>
 </div>
 
